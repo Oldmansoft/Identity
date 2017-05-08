f5a91f7746472da479a52bc3d9bdeb85aebc1efd
Oldman <angelialsong@gmail.com>
2017-05-08 17:22:26 +0800

    重命名项目名称。

--- src/Oldmansoft.Identity.Mvc/Properties/AssemblyInfo.cs
+++ src/Oldmansoft.Identity.Mvc/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Oldmansoft.Identity.MvcNet")]
+[assembly: AssemblyTitle("Oldmansoft.Identity.Mvc")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Oldmansoft.Identity.MvcNet")]
+[assembly: AssemblyProduct("Oldmansoft.Identity.Mvc")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
